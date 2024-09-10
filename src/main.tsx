2f16f657b359b86b571504cec2a0f5e3878e47b6
Denis <51379629+D3nya@users.noreply.github.com>
2024-09-10 15:00:30 +0300

    add: sass, router, db

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import router from "./router";
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
